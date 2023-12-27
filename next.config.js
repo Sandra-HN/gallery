cfe12059941314f12d1e8c65cb8a62e6e949e2b5
Sandra Hnaidy <sandra.hnaidy@dardoc.com>
2023-12-27 09:38:14 +0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // reactStrictMode: true,
+  // swcMinify: true,
+  // optimizeFonts: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dtrpgszja/**",
+      },
+    ],
+  },
+};
